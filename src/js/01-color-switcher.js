37d6647514bb0879a02269f895700d90a318112c
VadymMintenko <vad.m1ntenko@gmail.com>
2023-01-03 23:34:54 +0200

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let intervalId = null;
 
+buttonStart.addEventListener('click', onStart);
+buttonStop.addEventListener('click', onStop);
+
+function onStart() {
+  buttonStart.disabled = true;
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStop() {
+  clearInterval(intervalId);
+  buttonStart.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
