7f04ed72d550a9ef63e01b82de83f6e12a3130dd
VadymMintenko <vad.m1ntenko@gmail.com>
2023-01-08 00:49:25 +0200

    commit

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const formEl = document.querySelector('.form');
+formEl.addEventListener('input', onInput);
+
+const button = document.querySelector('button');
+button.addEventListener('click', onClick);
+const refs = {
+  amount: '',
+  delay: '',
+  step: '',
+};
+
+function onInput(evt) {
+  refs[evt.target.name] = evt.target.value;
+}
+
+function onClick() {
+  for (let i = 0; i < refs.amount; i += 1) {
+    console.log(i);
+    createPromise(2, 3000)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        res(position, delay);
+      } else {
+        rej(position, delay);
+      }
+    }, delay);
+  });
+}
+// createPromise(3, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
